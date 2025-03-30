7456cc5360469124aa7f59cf23b02f4211b19e1a
thatmarcel <contact@thatmarcel.com>
2025-03-31 01:28:33 +0200

    Increase version (v0.0.7 => v0.0.8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thatmarcel.apps.railochrone"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
     }
 
     buildTypes {
